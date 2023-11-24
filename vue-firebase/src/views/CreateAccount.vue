f56592fb097ec9f9a2c93e3f1a5f284289af52de
Harry081191 <harry081191@email.address>
2023-11-24 16:58:37 +0800

    1

--- vue-firebase/src/views/CreateAccount.vue
+++ vue-firebase/src/views/CreateAccount.vue
@@ -12,22 +12,22 @@
             <div class="jumbotron">
               <form @submit.prevent="handleSubmit">
                 <h1>註冊帳號</h1>
-                <strong>電子郵件</strong>
-                <input v-model="User.email" type="text" class="form-control" id="Email" name="Email" placeholder="請輸入電子郵件"
+                <strong class="font">電子郵件</strong>
+                <input v-model="User.email" type="text" class="form-control font" id="Email" name="Email" placeholder="請輸入電子郵件"
                   required>
-                <strong>帳號</strong>
-                <input v-model="User.username" type="text" class="form-control" id="Username" name="Username"
+                <strong class="font">帳號</strong>
+                <input v-model="User.username" type="text" class="form-control font" id="Username" name="Username"
                   placeholder="請輸入帳號" required>
-                <strong>密碼</strong>
-                <input v-model="User.password" type="password" class="form-control" id="Password" name="Password"
+                <strong class="font">密碼</strong>
+                <input v-model="User.password" type="password" class="form-control font" id="Password" name="Password"
                   placeholder="請輸入密碼" required>
                 <div style="margin-top: 10px; margin-bottom: 10px;">
-                  <button type="submit" class="btn btn-outline-danger">
+                  <button type="submit" class="btn btn-outline-danger font">
                     註冊
                   </button>
                 </div>
                 <div>
-                  <a class="from-control mr-2 mb-1 mb-sm-0">I have the account return to</a>
+                  <a class="from-control mr-2 mb-1 mb-sm-0 font">I have the account return to</a>
                   <router-link to="/" class="from-control mr-2 mb-1 mb-sm-0">Login</router-link>
                 </div>
               </form>
@@ -36,10 +36,21 @@
         </div>
       </div>
     </div>
+    <div v-if="data">
+      <ul>
+        <li v-for="(item, key) in data" :key="key">
+          {{ key }}: {{ item }}
+        </li>
+      </ul>
+    </div>
     <div v-if="errorMessage" class="alert alert-danger">{{ errorMessage }}</div>
   </div>
 </template>
-<style></style>
+<style>
+.font{
+  font-family: 微軟正黑體;
+}
+</style>
 <script>
 import { getAuth, createUserWithEmailAndPassword, sendEmailVerification, onAuthStateChanged } from 'firebase/auth';
 import { getDatabase, ref as firebaseRef, onValue, set } from 'firebase/database';
@@ -61,6 +72,11 @@ export default {
       this.dataLength = this.dataindex.length; // Store the length
       this.data = data; // Store the data in the component's data property
     });
+
+
+
+    console.log("Email verified. Calling onEmailVerified.");
+
     this.unsubscribeAuth = unsubscribeAuth;
   },
   data() {
