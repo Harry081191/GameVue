fbbdac96c41943c459544c63c77a65509a1f69dd
Harry081191 <harry081191@email.address>
2023-12-05 20:12:47 +0800

    1

--- vue-firebase/src/views/Home.vue
+++ vue-firebase/src/views/Home.vue
@@ -54,8 +54,13 @@
                 <p style="margin-bottom:15px;">最近遊玩(抓五個)</p>
                 <li style="margin-bottom:20px;" v-for="(item, key, index) in Recorddata" :key="key">
                   <template v-if="key !== 'TotalRecord' && index >= RecordLength - 5">
-                    <a>遊玩日期{{ key }}：</a> 等級：{{ item.Level }}／擊殺數：{{ item.killnumber }}／金幣：{{ item.money }}／遊玩時長：{{
-                      item.time }}<br>技能升級：{{ item.SkillUpgradeRecord }}
+                    <a>遊玩日期{{ key }}：</a> 等級：{{ item.Level }}／擊殺數：{{ item.killnumber }}／金幣：{{ item.money
+                    }}／遊玩時長：{{ item.time }}<br>
+                    技能升級： FireBallProject：{{ item.SkillUpgradeRecord.FireBallProject }}／getBloodExplode：{{
+                      item.SkillUpgradeRecord.getBloodExplode }}／getFireBall：{{ item.SkillUpgradeRecord.getFireBall
+                      }}／getFlameJet：{{ item.SkillUpgradeRecord.getFlameJet }}／getLightningBlast：{{
+                      item.SkillUpgradeRecord.getLightningBlast }}／getMagicWeapon：{{ item.SkillUpgradeRecord.getMagicWeapon
+                      }}
                   </template>
                 </li>
               </ul>
@@ -67,7 +72,12 @@
                 <p style="margin-top:15px; margin-bottom:15px;">存活時間最長(抓三個)</p>
                 <li style="margin-bottom:20px;" v-for="(item, key) in Recorddata" :key="key">
                   <template v-if="key !== 'TotalRecord'"><a>遊玩日期{{ key }}：</a> 等級：{{ item.Level }}／擊殺數：{{
-                    item.killnumber }}／金幣：{{ item.money }}／遊玩時長：{{ item.time }}<br>技能升級：{{ item.SkillUpgradeRecord }}
+                    item.killnumber }}／金幣：{{ item.money }}／遊玩時長：{{ item.time }}<br>
+                    技能升級： FireBallProject：{{ item.SkillUpgradeRecord.FireBallProject }}／getBloodExplode：{{
+                      item.SkillUpgradeRecord.getBloodExplode }}／getFireBall：{{ item.SkillUpgradeRecord.getFireBall
+                      }}／getFlameJet：{{ item.SkillUpgradeRecord.getFlameJet }}／getLightningBlast：{{
+                      item.SkillUpgradeRecord.getLightningBlast }}／getMagicWeapon：{{ item.SkillUpgradeRecord.getMagicWeapon
+                      }}
                   </template>
                 </li>
               </ul>
