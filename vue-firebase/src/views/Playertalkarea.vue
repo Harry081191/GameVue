7ac1e540c73d1bec64686874e3ef76efe2df449f
Harry081191 <harry081191@email.address>
2023-12-12 21:09:35 +0800

    1

--- vue-firebase/src/views/Playertalkarea.vue
+++ vue-firebase/src/views/Playertalkarea.vue
@@ -71,7 +71,7 @@
                           <ul v-if="menuStates[index]">
                             <li><button type="button"
                                 style="margin-bottom:2.5px; margin-top:2.5px; width: 60px; height: 25px;"
-                                :class="{ deleted: !deletePosts[index] }" @click="toggleReport()"><i
+                                :class="{ deleted: !deletePosts[index] || UserManager }" @click="toggleReport()"><i
                                   class="fas fa-exclamation-triangle"></i> 舉報</button>
                               <Transition>
                                 <div v-if="reportForm" class="form-container">
@@ -154,9 +154,9 @@
                               </Transition>
                               <div v-if="reportForm" class="overlay" @click="toggleReport(index)"></div>
                             </li>
-                            <li><button type="button"
+                            <li v-if="true"><button type="button"
                                 style="margin-bottom:2.5px; margin-top:2.5px; width: 60px; height: 25px;"
-                                :class="{ deleted: deletePosts[index] }" @click="toggleEdit(index)"><i
+                                :class="{ deleted: deletePosts[index] && !UserManager }" @click="toggleEdit(index)"><i
                                   class="far fa-edit"></i> 編輯</button>
                               <Transition>
                                 <div v-if="editForm" class="form-container">
@@ -189,17 +189,17 @@
                             </li>
                             <li><button type="button"
                                 style="margin-bottom:2.5px; margin-top:2.5px; width: 60px; height: 25px;"
-                                :class="{ deleted: deletePosts[index] }" @click="toggleDelete(index)"><i
+                                :class="{ deleted: deletePosts[index] && !UserManager }" @click="toggleDelete(index)"><i
                                   class="fas fa-exclamation-triangle"></i> 封鎖</button>
                             </li>
                             <li><button type="button"
                                 style="margin-bottom:2.5px; margin-top:2.5px; width: 60px; height: 25px;"
-                                :class="{ deleted: deletePosts[index] }" @click="toggleDelete(index)"><i
+                                :class="{ deleted: deletePosts[index] && !UserManager }" @click="toggleDelete(index)"><i
                                   class="far fa-trash-alt"></i> 解封</button>
                             </li>
                             <li><button type="button"
                                 style="margin-bottom:2.5px; margin-top:2.5px; width: 60px; height: 25px;"
-                                :class="{ deleted: deletePosts[index] }" @click="toggleDelete(index)"><i
+                                :class="{ deleted: deletePosts[index] && !UserManager }" @click="toggleDelete(index)"><i
                                   class="far fa-trash-alt"></i> 刪除</button>
                             </li>
                           </ul>
